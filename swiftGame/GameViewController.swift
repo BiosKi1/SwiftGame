8df5303d6ae13b81e03bd29ab0ccc3c37ce5a624
Renji333 <francischristanval@outlook.fr>
2018-06-19 16:15:55 +0200

    Boss Animation

--- swiftGame/GameViewController.swift
+++ swiftGame/GameViewController.swift
@@ -15,30 +15,23 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView?
+        {
+            // Load the SKScene from 'GameScene.sks'
+            //let scene = SKScene(fileNamed: "GameScene")
+            //let scene = GameScene.init(size: CGSize.init(width: 1136, height: 640))
+            let scene = SKScene(fileNamed: "GameScene")
+            // Set the scale mode to scale to fit the window
+            scene!.scaleMode = .aspectFill
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            // Present the scene
+            view.presentScene(scene!)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
     }
 
